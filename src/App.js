6ba5849e3805cf696f388415908772f7c755f6d7
Karina Lund <toyotac7@gmail.com>
2018-07-29 21:13:16 +0200

    filter function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import NeighborhoodMap from './NeighborhoodMap'
 import LocationList from './LocationList'
-import { push as Menu } from 'react-burger-menu'
+
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.map;
+    // this.map;
     this.initMap = this.initMap.bind(this);
     this.createMarkers = this.createMarkers.bind(this);
     this.geocodeCity = this.geocodeCity.bind(this);
     this.callback = this.callback.bind(this);
+    this.addPlace = this.addPlace.bind(this);
+    this.filterPlaces = this.filterPlaces.bind(this);
   }
 
 
   state = {
+    map: {},
     cityName: "Stavanger",
     cityLocation: {},
     places: []
@@ -24,21 +27,56 @@ class App extends Component {
   componentDidMount() {
     window.initMap = this.initMap;
     loadJS('https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyD0MMgs8WPQip9ftHRDeONPJxH8ZgjzYgs&callback=initMap')
-    // this.setState({ cityLocation:  });
-    // console.log(this.cityLocation);
+  }
+
+  addPlace(place, marker) {
+    place.marker = marker;
+    this.setState(state => ({
+      places: state.places.concat([place])
+    }));
+  }
+
+  filterPlaces(inputText) {
+    console.log('filter');
+    inputText = inputText.toLowerCase();
+    // let filteredPlaces = [];
+    this.state.places.forEach(place => {
+        if (place.name.toLowerCase().indexOf(inputText) > -1 || inputText === "") {
+            place.marker.setVisible(true);
+            // place.visible = true;
+            // filteredPlaces.push(place);
+        } else {
+            place.marker.setVisible(false);
+            // place.visible = false;
+        }
+    });
+    this.forceUpdate()
+    // console.log(this.state.places.filter(place => place.name.toLowerCase().indexOf(inputText) > -1));
+    // console.log(inputText);
+
+    // if (inputText == "") {
+    //   this.setState({ visiblePlaces: this.state.places });
+    // } else {
+    //   this.setState(state => ({
+    //     visiblePlaces: state.places.filter(place => place.name.toLowerCase().indexOf(inputText) > -1)
+    //   }));
+    // }
+
+    // if (inputText == "") {
+    //   this.setState({ visiblePlaces: this.state.places });
+    // } else {
+    //   this.setState(state => ({
+    //     visiblePlaces: state.places.filter(place => place.name.toLowerCase().indexOf(inputText) > -1)
+    //   }));
+    // }
+
+
   }
 
   initMap() {
     // let map;
     let service;
     let self = this;
-    // this.geocodeCity();
-    // this.findNearestBars();
-
-    this.map = new window.google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
-        center: {lat: 51.509865, lng: -0.118092}
-    });
     var requestGeocode = {'address': this.state.cityName};
     var geocoder = new window.google.maps.Geocoder();
     geocoder.geocode(requestGeocode, this.geocodeCallback.bind(this));
@@ -47,6 +85,12 @@ class App extends Component {
   geocodeCallback(results, status) {
     let self = this;
     if (status === 'OK') {
+      // Creating map, based on city location
+      this.map = new window.google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: results[0].geometry.location
+      });
+
       let requestNearestBar = {
         location: results[0].geometry.location,
         radius: '300',
@@ -58,39 +102,83 @@ class App extends Component {
   }
 
   callback(results, status) {
-    let self = this;
-    if (status == window.google.maps.places.PlacesServiceStatus.OK) {
-      // this.setState({virtualMarkers: virtMarker});
-      this.createMarkers(results);
-    }
-}
+      if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+        // this.setState({places: results});
+        // this.initPlaces(results);
+        // this.createMarkers();
+        this.createMarkers(results);
+      }
+  }
+
+  // initPlaces(results) {
+  //   // Saving just N places from results
+  //   let self = this;
+  //   // let somePlaces = [];
+  //   for (var i = 0, place; place = results[i]; i++) {
+  //     if (i < 15) {
+  //       // somePlaces.push(place);
+  //       self.addPlace(place);
+  //     }
+  //   }
+  //   // this.setState(state => ({
+  //   //   places: somePlaces,
+  //   //   visiblePlaces: somePlaces
+  //   // }));
+  // }
+  //
+  // createMarkers() {
+  //   let self = this;
+  //   var bounds = new window.google.maps.LatLngBounds();
+  //
+  //   for (var i = 0, place; place = this.state.places[i]; i++) {
+  //
+  //       var image = {
+  //         url: place.icon,
+  //         size: new window.google.maps.Size(71, 71),
+  //         origin: new window.google.maps.Point(0, 0),
+  //         anchor: new window.google.maps.Point(17, 34),
+  //         scaledSize: new window.google.maps.Size(25, 25)
+  //       };
+  //
+  //       var marker = new window.google.maps.Marker({
+  //         map: self.map,
+  //         icon: image,
+  //         title: place.name,
+  //         position: place.geometry.location
+  //       });
+  //       bounds.extend(place.geometry.location);
+  //   }
+  //   self.map.fitBounds(bounds);
+  // }
 
 createMarkers(places) {
   let self = this;
   var bounds = new window.google.maps.LatLngBounds();
-  var placesList = document.getElementById('places');
 
+  // Saving just N places.
   for (var i = 0, place; place = places[i]; i++) {
-    var image = {
-      url: place.icon,
-      size: new window.google.maps.Size(71, 71),
-      origin: new window.google.maps.Point(0, 0),
-      anchor: new window.google.maps.Point(17, 34),
-      scaledSize: new window.google.maps.Size(25, 25)
-    };
+    if (i < 15) {
+      var image = {
+        url: place.icon,
+        size: new window.google.maps.Size(71, 71),
+        origin: new window.google.maps.Point(0, 0),
+        anchor: new window.google.maps.Point(17, 34),
+        scaledSize: new window.google.maps.Size(25, 25)
+      };
 
-    var marker = new window.google.maps.Marker({
-      map: self.map,
-      icon: image,
-      title: place.name,
-      position: place.geometry.location
-    });
+      var marker = new window.google.maps.Marker({
+        map: self.map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+      // self.state.markers.concat(marker);
+      self.addPlace(place, marker);
+      // console.log(place);
+      // console.log(marker);
+      bounds.extend(place.geometry.location);
+    }
 
-    // var li = document.createElement('li');
-    // li.textContent = place.name;
-    // placesList.appendChild(li);
-
-    bounds.extend(place.geometry.location);
   }
   self.map.fitBounds(bounds);
 }
@@ -132,18 +220,9 @@ createMarkers(places) {
     return (
 
       <div id="outer-container">
-        <Menu
-          isOpen
-          noOverlay
-          pageWrapId={ "page-wrap" }
-          outerContainerId={ "outer-container" }>
-          <div>
-            <input id="places-search" type="text" placeholder="Bar name" />
-            <button id="filter-btn"><i className="fa fa-filter"></i> Filter</button>
-          </div>
-          <a id="home" className="menu-item" href="/">Home</a>
-
-        </Menu>
+        <LocationList
+          places={this.state.places}
+          onFilterPlaces={this.filterPlaces}/>
         <main id="page-wrap">
           <header>
             <h1>Bars in Stavanger</h1>
--- src/LocationList.js
+++ src/LocationList.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
-
+import { push as Menu } from 'react-burger-menu'
 class LocationList extends Component {
 
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    // alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+    if (this.props.onFilterPlaces) {
+      this.props.onFilterPlaces(this.state.value);
+    }
+  }
+
+  handleKeyPress = (event) => {
+  if (event.key === 'Enter') {
+    this.handleSubmit(event);
+  }
+}
+
   render() {
+    const {places} = this.props;
+
     return (
-      <nav>
-        <ul class="links">
-          <li> <a href="#">Home</a> </li>
-          <li> <a href="#">About</a> </li>
-          <li> <a href="#">Projects</a> </li>
-          <li> <a href="#">Blog</a> </li>
-          <li> <a href="#">Contacts</a> </li>
-        </ul>
-      </nav>
+      <Menu
+        isOpen
+        noOverlay
+        pageWrapId={ "page-wrap" }
+        outerContainerId={ "outer-container" }>
+        <div>
+          <input id="places-filter" type="text" placeholder="Bar name" value={this.state.value} onChange={this.handleChange} onKeyPress={this.handleKeyPress} />
+          <button id="filter-btn" onClick={this.handleSubmit}><i className="fa fa-filter"></i> Filter</button>
+        </div>
+        {places.filter(place => place.marker.getVisible() === true).map(place => (
+          <a key={place.name} className="menu-item">
+            {place.name}
+          </a>
+        ))}
+
+
+      </Menu>
     );
   }
 }
